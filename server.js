344dc80136e445055fb2ca1f142717bdd65e55bd
Jstuff36 <mizzou_fan10@hotmail.com>
2017-08-05 15:58:26 -0700

    Connect to mongo database

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
-console.log('May Node be with you');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
 
-app.listen(3000, () => {
-  console.log('listening on 3000');
-});
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/quotes', (req, res) => {
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err);
+
+    console.log('saved to database');
+    res.redirect('/');
+  });
+  console.log(req.body);
+});
+
+var db;
+
+MongoClient.connect('mongodb://Jar Jar Binks:password123@ds129023.mlab.com:29023/star-wars-quotes', (err, database) => {
+  if (err) return console.log(err);
+  db = database;
+  app.listen(3000, () => {
+    console.log('listening on 3000');
+  });
 });
