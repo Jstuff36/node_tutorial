e930f83bd3326120de3384aaf790f991ad964767
Jstuff36 <mizzou_fan10@hotmail.com>
2017-08-05 17:42:51 -0700

    Add patch and delete methods

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient;
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   db.collection('quotes').find().toArray( (err, result) => {
@@ -21,6 +23,34 @@ app.post('/quotes', (req, res) => {
     res.redirect('/');
   });
   console.log(req.body);
+});
+
+app.put('/quotes', (req, res) => {
+  db.collection('quotes')
+  .findOneAndUpdate({name: 'Yoda'}, {
+    $set: {
+      name: req.body.name,
+      quote: req.body.quote
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err);
+    res.send(result);
+  });
+});
+
+app.delete('/quotes', (req, res) => {
+  db.collection('quotes').findOneAndDelete(
+    {
+      name: req.body.name
+    },
+    (err, result) => {
+      if (err) return res.send(500, err);
+      res.send({message: 'A darth vadar quote got deleted'});
+    }
+  );
 });
 
 var db;
